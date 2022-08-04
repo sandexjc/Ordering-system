09e47f6fcc799e7361e38d5c685b4bd85e74ddc5
Aleksandar <aleksandar.boyadzhiev2@gmail.com>
2022-08-04 11:57:48 +0300

    Changes in Externals tab

--- Tracking Table/SimpleTable/views.py
+++ Tracking Table/SimpleTable/views.py
@@ -37,25 +37,21 @@ class Internals(LoginRequiredMixin, TemplateView):
             internals_ids = Order.objects.filter(
                 client='Internal', 
                 ID__contains=kwargs['search_string'])
-            # print(internals_ids)
             results.append(internals_ids)
 
             internals_date = Order.objects.filter(
                 client='Internal', 
                 created_date__contains=kwargs['search_string'])
-            # print(internals_date)
             results.append(internals_date)
 
             internals_name = Order.objects.filter(
                 client='Internal', 
                 owner__contains=kwargs['search_string'])
-            # print(internals_name)
             results.append(internals_name)
 
             internals_telephone = Order.objects.filter(
                 client='Internal', 
                 telephone__contains=kwargs['search_string'])
-            # print(internals_telephone)
             results.append(internals_telephone)
 
             for item in results:
@@ -63,7 +59,6 @@ class Internals(LoginRequiredMixin, TemplateView):
                     for res in item:
                         if res not in all_internals:
                             all_internals.append(res)
-                            # print(f'APPENDED {res}')
 
             if len(kwargs['search_string']) == 0:
                 context['badges'] = False
@@ -99,25 +94,29 @@ class Externals(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(Externals, self).get_context_data(**kwargs)
-        plate_forms = []
-        edge_forms = []
+        context['externals'] = []
 
-        class ProgressForm:
+        class OrderObject:
 
-            def __init__(self, order_id, form):
-                self.order_id = order_id
-                self.form = form
+            def __init__(self, external):
+                self.order = external
+                self.material_eger = Plate.objects.filter(cutID=external, manufacturer='Egger')
+                self.material_krono = Plate.objects.filter(cutID=external, manufacturer='Kronospan')
+                self.material_edge = Edge.objects.filter(cutID=external)
+                self.notes = Note.objects.filter(cutID=external)
+                self.plate_forms = forms.PlateProgressFormSet(instance=external)
+                self.edge_forms = forms.EdgeProgressFormSet(instance=external)
 
             def __str__(self):
-                return self.form
+                return str(self.order)
+
 
         if self.request.POST:
-            # print(f'SEARCHED {kwargs["search_string"]}')
             context['search_form'] = forms.SearchForm(self.request.POST)
             context['search_string'] = kwargs["search_string"]
 
             results = []
-            externals = []
+            all_externals = []
 
             externals_ids = Order.objects.filter(
                 client='External', 
@@ -146,39 +145,20 @@ class Externals(LoginRequiredMixin, TemplateView):
             for item in results:
                 if len(item) != 0:
                     for res in item:
-                        if res not in externals:
-                            externals.append(res)
-                            # print(f'APPENDED {res}')
+                        if res not in all_externals:
+                            all_externals.append(res)
 
             if len(kwargs['search_string']) == 0:
                 context['badges'] = False
             else:
                 context['badges'] = True
         else:
-            externals = Order.objects.filter(client='External')
+            all_externals = Order.objects.filter(client='External')
             context['search_form'] = forms.SearchForm
             context['badges'] = False
 
-        for external in externals:
-
-            external.material_eger = Plate.objects.filter(cutID=external.pk, manufacturer='Egger')
-            external.material_krono = Plate.objects.filter(cutID=external.pk, manufacturer='Kronospan')
-            external.material_edge = Edge.objects.filter(cutID=external.pk)
-            external.notes = Note.objects.filter(cutID=external.pk)
-
-            plate_form = forms.PlateProgressFormSet(instance=external)
-            form_object = ProgressForm(external.ID, plate_form)
-            plate_forms.append(form_object)
-
-            edge_form = forms.EdgeProgressFormSet(instance=external)
-            form_object = ProgressForm(external.ID, edge_form)
-            edge_forms.append(form_object)
-
-        # print(type(internals)) --> QUERY SET !!!
-        context['externals'] = externals
-        context['update_forms'] = plate_forms
-        context['edge_forms'] = edge_forms
-
+        for external in all_externals:
+            context['externals'].append(OrderObject(external))
 
         current_time = time.localtime(time.time())
 
