8c24794856f2d19d318cc23883d2ea6529bf5d31
Aleksandar <aleksandar.boyadzhiev2@gmail.com>
2023-01-03 12:36:41 +0200

    Adding Export function in Admin
    Adding export function in Admin center

--- Tracking Table/table/admin.py
+++ Tracking Table/table/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.fields import Field
+from import_export.widgets import ForeignKeyWidget
+from import_export.admin import ExportActionMixin
 from table import models
 
-admin.site.register(models.Order)
-admin.site.register(models.Plate)
+# class OrderResource(resources.ModelResource):
+
+# 	class Meta:
+# 		model = models.Order
+
+# class ExportAdmin(ExportActionMixin, admin.ModelAdmin):
+# 	resource_classes = [OrderResource]
+
+
+class ExportAdmin(ExportActionMixin, admin.ModelAdmin):
+	pass
+
+admin.site.register(models.Order, ExportAdmin)
+admin.site.register(models.Plate, ExportAdmin)
 admin.site.register(models.Cutting)
 admin.site.register(models.Edge)
 admin.site.register(models.Edging)
