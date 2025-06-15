6daab06995dc6b3a48062d2a571e903ab6d309dc
Aleksandar <aleksandar.boyadzhiev2@gmail.com>
2025-06-16 02:35:55 +0300

    Organize code part-2 table app views

---
+++ Tracking Table/table/views/__init__.py
@@ -0,0 +1,7 @@
+from .CreateOrderView import CreateOrder
+from .DeleteOrderView import DeleteOrder
+from .EditOrderView import EditOrder
+from .PrintOrderView import PrintOrder
+from .UpdateOrderView import UpdateOrder
+from .OrderHistoryView import OrderHistory
+from .OrderDetailsView import ViewOrder
