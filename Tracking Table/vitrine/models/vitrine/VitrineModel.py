fde97b1d4d5797e2aa94432df86d0013ff3722d9
Aleksandar <aleksandar.boyadzhiev2@gmail.com>
2025-10-14 01:54:06 +0300

    Improvement - add vitrine moedel items total price fields

--- Tracking Table/vitrine/models/vitrine/VitrineModel.py
+++ Tracking Table/vitrine/models/vitrine/VitrineModel.py
@@ -6,6 +6,11 @@ class Vitrine(VitrineOrder, BaseOrder):
 
     id = models.BigAutoField(primary_key=True)
 
+    frame_total = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+    glass_total = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+    holes_total = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+    other_total = models.DecimalField(max_digits=15, decimal_places=2, default=0)
+    seals_total = models.DecimalField(max_digits=15, decimal_places=2, default=0)
     
     def __str__(self):
         return str(self.id)
