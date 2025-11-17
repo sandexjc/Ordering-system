3fcdf55519da341a4063355dcf2e6d752019b4ad
Aleksandar <aleksandar.boyadzhiev2@gmail.com>
2025-11-17 13:55:45 +0200

    Improvement - add Vitrine app models into admin page

--- Tracking Table/vitrine/admin.py
+++ Tracking Table/vitrine/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from vitrine import models
 
-# Register your models here.
+admin.site.register(models.Vitrine)
+admin.site.register(models.Frame)
+admin.site.register(models.Hole)
+admin.site.register(models.Glass)
+admin.site.register(models.Seal)
+admin.site.register(models.Other)
+admin.site.register(models.Note)
+admin.site.register(models.Change)
+admin.site.register(models.Payment)
