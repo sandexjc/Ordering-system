c108e394ffc8c9f3ac638d9ba3290effd8624e3e
Aleksandar <aleksandar.boyadzhiev2@gmail.com>
2025-12-03 13:27:33 +0200

    Refactor code - print page

--- Tracking Table/table/views/PrintOrderView.py
+++ Tracking Table/table/views/PrintOrderView.py
@@ -1,10 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
-
-from table.models import (
-    Order, Plate, Edge, Cutting,
-    Edging, Other, Payment
-)
+from table.models import Order
 
 class PrintOrder(LoginRequiredMixin, TemplateView):
     model = Order
@@ -12,13 +8,6 @@ class PrintOrder(LoginRequiredMixin, TemplateView):
     
     def get_context_data(self, pk, **kwargs):
         context = super(PrintOrder, self).get_context_data(**kwargs)
-
         context['order'] = Order.objects.get_by_id(pk)
-        context['order_plates'] = Plate.objects.for_order_id(pk)
-        context['order_edges'] = Edge.objects.for_order_id(pk)
-        context['order_cutting'] = Cutting.objects.for_order_id(pk)
-        context['order_edging'] = Edging.objects.for_order_id(pk)
-        context['order_others'] = Other.objects.for_order_id(pk)
-        context['order_payments'] = Payment.objects.for_order_id(pk)
 
         return context
