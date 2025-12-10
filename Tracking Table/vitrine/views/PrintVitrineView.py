aa3dca0b7a6cd645b0b798bd6dfce5310d1d1970
Aleksandar <aleksandar.boyadzhiev2@gmail.com>
2025-12-10 14:02:28 +0200

    Improvement - complete vitrine app print view information

--- Tracking Table/vitrine/views/PrintVitrineView.py
+++ Tracking Table/vitrine/views/PrintVitrineView.py
@@ -1,13 +1,17 @@
 from common.views import BasePrintView
 from vitrine.models import Vitrine
+from vitrine.service import VitrineContextBuilder
 
 class PrintVitrine(BasePrintView):
-    
+
     model = Vitrine
     template_name = 'vitrine/print_vitrine.html'
     
     def get_context_data(self, pk, **kwargs):
         context = super(PrintVitrine, self).get_context_data(**kwargs)
-        context['vitrine'] = Vitrine.objects.get_by_id(pk)
+        vitrine = Vitrine.objects.get_by_id(pk)
+        
+        context['vitrine'] = vitrine
+        context.update(VitrineContextBuilder.build(vitrine))
 
         return context
